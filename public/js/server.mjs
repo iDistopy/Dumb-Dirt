d4d9ceab681057d2262554a791b9d90a454b034b
Esteban Guerra <144506726+iDistopy@users.noreply.github.com>
2024-05-13 16:43:23 -0400

    fix for use in another desktops
    Now is "working" only the stylesheet. JS not, need to fix it

--- public/js/server.mjs
+++ public/js/server.mjs
@@ -13,14 +13,16 @@ const app = express();
 app.use(cors());
 
 // Configura el directorio para archivos estáticos
-app.use('/css', express.static(path.join('css')));
-app.use('/js', express.static(path.join('js')));
-app.use('/utils', express.static(path.join('utils')));
-app.use('/home', express.static(path.join('home')));
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+app.use('/css', express.static(path.join(__dirname, '..', 'js')));
+app.use('/utils', express.static(path.join(__dirname, '..', 'utils')));
+app.use('/home', express.static(path.join(__dirname, '..', 'home')));
 
 // Define la ruta base para la carpeta /home/
 app.get('/inicio', (req, res) => {
+  res.type('text/html');
   res.sendFile(path.resolve(__dirname, '..', 'home', 'inicio.html'));
+  console.log(path.resolve(__dirname, '..', 'home', 'inicio.html'));
 });
 
 
